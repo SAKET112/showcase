4a9658b15e57addfd6455f88ea520e1115b413c2
LUCKY RANJAN <ranjan.lucky543@gmail.com>
2023-09-27 02:13:49 +0530

    first commit

---
+++ src/components/Download.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+import styles from "../styles/Global";
+import assets from "../assets";
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <dir className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the full Source Code on Github</p>
+        </div>
+        <button className={styles.btnPrimary}>Source Code</button>
+        <div className={styles.flexCenter}>
+          <img src={assets.scene} alt="scene" className={styles.fullImg} />
+        </div>
+      </dir>
+    </div>
+  )
+}
+
+export default Download
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import SectionWrapper from './SectionWrapper';
+import Features from './Features';
+import Download from './Download';
+
+export {SectionWrapper, Features, Download};
